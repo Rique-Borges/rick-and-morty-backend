42a65817aa63287f38d30556857d6b67701a04bd
Rique-Borges <henriqueribeiroborges@gmail.com>
2025-10-22 21:07:18 -0300

    add: login, autenticação e authguard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, UseGuards, Get, Request, } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
-
+import { LoginUserDto } from './dto/login-user.dto';
+import { AuthGuard } from './auth/auth.guard';
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService){}
@@ -10,4 +11,17 @@ export class AuthController {
     register(@Body() registerUserDto: RegisterUserDto){
         return this.authService.register(registerUserDto)
     }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    login(@Body() loginUserDto: LoginUserDto){
+        return this.authService.login(loginUserDto)
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req){
+        return req.user;
+    }
+    
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,34 +1,62 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import * as bcrypt from 'bcrypt';
-
+import { JwtService } from '@nestjs/jwt';
+import { LoginUserDto } from './dto/login-user.dto';
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService) {}
 
   async register(registerUserDto: RegisterUserDto) {
     const { name, email, password } = registerUserDto;
 
-    // 1. Verificar se o usuário já existe
+    //Verificar se o usuário já existe
     const existingUser = await this.usersService.findByEmail(email);
     if (existingUser) {
-      throw new ConflictException('User with this email already exists');
+      throw new ConflictException('Já existe um usuário com este email');
     }
 
-    // 2. Criptografar a senha
+    //Criptografar a senha
     const hashedPassword = await bcrypt.hash(password, 10); // O '10' é o salt round
 
-    // 3. Criar o novo usuário
+    //Criar o novo usuário
     const user = await this.usersService.create({
         name,
         email,
         password: hashedPassword,
     });
 
-    // 4. Retornar o usuário criado (sem a senha)
-    // eext-line @typescript-eslint/no-unused-vars
+    //Retornar o usuário criado (sem a senha)
     const { password: _, ...result } = user;
     return result;
   }
+
+
+  async login(loginUserDto: LoginUserDto){
+    const {email, password} = loginUserDto
+
+    //Encontrar o User pelo email
+    const user = await this.usersService.findByEmail(email);
+    if(!user) {
+      throw new UnauthorizedException('Credenciais Inválidas')
+    }
+
+    //Comparar senhas
+    const isPasswordMatching = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatching) {
+      throw new UnauthorizedException('Credenciais Inválidas')
+    }
+
+    //Gerar payload do JWT
+    const payload = {sub: user.id, email: user.email};
+
+    //Assinar e retornar o token
+    return{
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+
 }
